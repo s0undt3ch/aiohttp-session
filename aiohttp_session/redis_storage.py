6f0b1071b1d29fb7d5b505016cddffe98243740a
Pedro Algarvio <pedro@algarvio.me>
2016-12-09 16:46:47 +0000

    Only async/await syntax

--- aiohttp_session/redis_storage.py
+++ aiohttp_session/redis_storage.py
@@ -1,4 +1,3 @@
-import asyncio
 import json
 import uuid
 
@@ -19,15 +18,14 @@ class RedisStorage(AbstractStorage):
         self._decoder = decoder
         self._redis = redis_pool
 
-    @asyncio.coroutine
-    def load_session(self, request):
+    async def load_session(self, request):
         cookie = self.load_cookie(request)
         if cookie is None:
             return Session(None, data=None, new=True, max_age=self.max_age)
         else:
-            with (yield from self._redis) as conn:
+            with await self._redis as conn:
                 key = str(cookie)
-                data = yield from conn.get(self.cookie_name + '_' + key)
+                data = await conn.get(self.cookie_name + '_' + key)
                 if data is None:
                     return Session(None, data=None,
                                    new=True, max_age=self.max_age)
@@ -35,8 +33,7 @@ class RedisStorage(AbstractStorage):
                 data = self._decoder(data)
                 return Session(key, data=data, new=False, max_age=self.max_age)
 
-    @asyncio.coroutine
-    def save_session(self, request, response, session):
+    async def save_session(self, request, response, session):
         key = session.identity
         if key is None:
             key = uuid.uuid4().hex
@@ -48,8 +45,8 @@ class RedisStorage(AbstractStorage):
                              max_age=session.max_age)
 
         data = self._encoder(self._get_session_data(session))
-        with (yield from self._redis) as conn:
+        with await self._redis as conn:
             max_age = session.max_age
             expire = max_age if max_age is not None else 0
-            yield from conn.set(self.cookie_name + '_' + key,
-                                data, expire=expire)
+            await conn.set(self.cookie_name + '_' + key,
+                           data, expire=expire)
